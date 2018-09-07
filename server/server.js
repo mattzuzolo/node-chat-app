a760c350f5fc0eb7992972a1ca0a0bd51ea5de92
github username <github email address>
2018-09-07 18:29:11 -0400

    added connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -1,13 +1,27 @@
 const path = require("path");
+const http = require("http");
 const express = require("express");
+const socketIO = require("socket.io");
 
 const publicPath = path.join(__dirname, "../public");
 const PORT = process.env.PORT || 3000;
 let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
 
 //add middleware to express
 app.use(express.static(publicPath));
 
-app.listen(PORT, () => {
+//register event listener
+//listen for connection and do something in CB
+io.on("connection", (socket) => {
+  console.log("New user connected");
+
+  socket.on("disconnect", () => {
+    console.log("User was disconnected")
+  })
+});
+
+server.listen(PORT, () => {
   console.log(`Server is up on port ${PORT}`);
 });
